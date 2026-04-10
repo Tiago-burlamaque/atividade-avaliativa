73b1da0c6e88803c60f377f557f2a89b16455349
tiago-burlamaque <tiagoburlamaque79@gmail.com>
2026-04-09 21:30:32 -0300

    ola

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import Cadastro from './pages/cadastro.jsx';
+import Login from './pages/login.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+
+const router = createBrowserRouter([
+  { path: "/", element: <Login /> },
+  { path: "/cadastro", element: <Cadastro /> },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />,
+  </StrictMode>,
+)
